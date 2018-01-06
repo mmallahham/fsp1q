9d16d12f8f59290603b735290c00eb0d3f5985a6
mmallahham <mmallahham@hotmail.com>
2018-01-06 15:48:40 +0100

    second

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-var app = require('express');
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var open = require('open');
+require('jquery');
+
+var app = express();
+var port = 3000;
+
+app.get('/',function(req,res){
+  console.log('dfgdf');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/jquery-3.2.1.js',function(req,res){
+  console.log('jq');
+  res.sendFile(path.join(__dirname, 'jquery-3.2.1.js'));
+});
 
 
-
-app.get('/',function(){
-    
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
 });
